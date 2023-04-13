9b5d013379f23b1c9ef7534bddcab7accb6f9fc1
MrBronzBonz <123484450+MrBronzBonz@users.noreply.github.com>
2023-04-13 08:49:42 +0200

    Update checkformatofile.c

--- checkformatofile.c
+++ checkformatofile.c
@@ -49,6 +49,25 @@ int fdf_check(char *file)
     return(0);
 }
 
+//dopo il suggerimento di partire dalla fine ho migliorato la funzione precedente:
+
+/*int fdf_check(char *file)
+{
+    int i;
+
+    i = 0;
+    while (file[i] != '\0')
+    {
+        if(file[i] == '.' && file[i+1] == 'f' && file[i+2] == 'd' &&
+                file[i+3] == 'f' && file[i+4] == '\0')
+            return (0);
+        else
+            i++;
+    }
+    printf("file formato sbagliato");
+    return (0);
+}*/
+
 int main(int argc, char **argv)
 {
     if(argc == 2)
